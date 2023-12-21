81cd724b6ce117d2f871c10c6b5693abc9eeaf41
IRFU <IRFU@DESKTOP-2MARTPS.attlocal.net>
2023-12-20 18:30:00 -0800

    Finished with Fizz Buzz

--- src/_01_else_if/_5_own_adventure/OwnAdventure.java
+++ src/_01_else_if/_5_own_adventure/OwnAdventure.java
@@ -4,9 +4,24 @@ import javax.swing.JOptionPane;
 
 public class OwnAdventure {
 	public static void main(String[]args) {
-		String story = JOptionPane.showInputDialog("Are you a fan of the Lakers");
-		if(story.equals("yes")) {
-			JOptionPane.showInputDialog("");
+		String story = JOptionPane.showInputDialog("Who is the GOAT Jordan or LeBron?");
+		if(story.equals("Jordan")) {
+			String j_champ = JOptionPane.showInputDialog("Did Michael Jordan win 6 championships or 5?");
+			if(j_champ.equals("6")) {
+				JOptionPane.showMessageDialog(null, "Jordan is the GOAT");
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "You are not a Jordan fan");
+			}
+		}
+		else {
+			String l_champ = JOptionPane.showInputDialog("Did LeBron James win 4 championships or 5");
+			if(l_champ.equals("4")) {
+				JOptionPane.showMessageDialog(null, "LeBron is the GOAT");
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "You are not a LeBron fan");
+			}
 		}
 	}
 }
---
+++ src/_02_loop_variables/_4_99_bottles/bottles/bottles.pde
@@ -0,0 +1,5 @@
+for(int i=99; i>1; i--) {
+  System.out.println(i+ " bottles of beer on the wall");
+  System.out.println(i+ " bottles of beer");
+  System.out.println(i+ " Take one down, pass it around");
+}
