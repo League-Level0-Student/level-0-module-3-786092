1e44985143a1eb2e9947949d8b82f84363caff6b
IRFU <IRFU@DESKTOP-2MARTPS.attlocal.net>
2024-01-03 18:13:50 -0800

    Finished Module 3

--- src/_03_remainder/_4_fizz_buzz/FizzBuzz.java
+++ src/_03_remainder/_4_fizz_buzz/FizzBuzz.java
@@ -26,23 +26,10 @@ public class FizzBuzz {
 	 */
 	public static void main(String[] args) {
 		
-		for( int i = 1 ; i <= 20; i++ ) {
-			// Divisible by 3 and divisible by 5
-			if(i%3==0 && i%5==0) {
-				System.out.println("FizzBuzz");
-			}
-			// Divisible by 3
-			else if(i%3==0) { 
-				System.out.println("Fizz");
-			}
-			// Divisible by 5
-			else if(i%5==0) {
-				System.out.println("Buzz");
-			}
-			else {
-				System.out.println(i);
-			}
+		for(int i=0; i<21; i++) {
+			  if(i%2==0) {
+			     System.out.println(i);
+			  }
 		}
 	}
 }
-
